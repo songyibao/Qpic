5c9c6f006c894bc13457c15c27487fd12e20e978
SongYibao <songybmail@foxmail.com>
2023-12-05 17:45:24 +0800

    crop image doing

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 // config.js
-const serverUrl = 'http://10.202.221.49:5000'
+const serverUrl = 'http://10.142.25.246:5000'
 const superResolutionUrl = serverUrl + '/uploadImg/1';
 const pic2dUrl = serverUrl + '/uploadImg/2';
+const faceRestoreUrl = serverUrl + '/uploadImg/4';
 const styleTransferUrl = serverUrl + '/uploadImg/styleTransfer';
 export {
 	superResolutionUrl,
 	pic2dUrl,
-	styleTransferUrl
+	styleTransferUrl,
+	faceRestoreUrl
 };
